14caeb2022230241a00e841919b1af34b99edfc8
Pedro Morais <pedrojfmorais@hotmail.com>
2022-12-29 19:17:46 +0000

    correção empresa

--- TrabalhoPratico/Controllers/EmpresasController.cs
+++ TrabalhoPratico/Controllers/EmpresasController.cs
@@ -194,7 +194,7 @@ namespace TrabalhoPratico.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,EstadoSubscricao,Classificacao")] Empresa empresa)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,EstadoSubscricao")] Empresa empresa)
         {
             if (id != empresa.Id)
             {
